5e12d427504a354dd65760b17c6a1f07896469de
leenawatH <leenawat.hon@student.mahidol.ac.th>
2022-04-03 01:05:58 +0700

    fix

--- Drive sound/web project/database/drive_sound.sql
+++ Drive sound/web project/database/drive_sound.sql
@@ -2,44 +2,33 @@ DROP DATABASE IF EXISTS drive_sound;
 create database drive_sound;
 use drive_sound;
 create table user_info(
-	user_id			int(5)    	 primary key,
+	user_id			varchar(5)   primary key,
     user_username	varchar(45)  not null,
-    user_email		varchar(50)  not null,
-    user_pass		varchar(50)  not null,
     user_fname		varchar(45)	 not null,
     user_lname		varchar(45)	 not null,
     user_bdate		datetime	 not null,
     user_tel		varchar(10)  not null,
-    user_role		char(1)		 not null,
     user_bio		varchar(1000) 
 );
-insert into user_info(user_id,user_username,user_email,user_pass,user_fname,user_lname,user_bdate,user_tel,user_role,user_bio) VALUES
-(00001,'Steve_Jo','steve_jolington@gmail.com','Sjo12345','Steve','Jolington','1985-1-20','0919998877','u','I am one of music lover and I want to share my opinion in many sound that I know'),
-(00002,'PPan01','peter_p@gmail.com','Ppan_01','Peter','Pan','1980-6-10','0834455667','a','My rating 4-5 - special/like my life, 3 - It great but not the best for me, 2 - just good, 0 - 1 - not ok for me'),
-(00003,'Li_Lo','lily_lon@gmail.com','Llon_zaza007','Lily','Lonatta','2002-10-20','0889988776','u','Satit to Muict, like photograhpy and drawing ^^');
+insert into user_info(user_id,user_username,user_fname,user_lname,user_bdate,user_tel,user_bio) VALUES
+('u0001','Steve_Jo','Steve','Jolington','1985-1-20','0919998877','I am one of music lover and I want to share my opinion in many sound that I know'),
+('a0002','PPan01','Peter','Pan','1980-6-10','0834455667','My rating 4-5 - special/like my life, 3 - It great but not the best for me, 2 - just good, 0 - 1 - not ok for me'),
+('u0003','Li_Lo','Lily','Lonatta','2002-10-20','0889988776','Satit to Muict, like photograhpy and drawing ^^');
 
-create table login_info(
-	login_email		varchar(50)  primary key,
-    login_userid	int(5)		 not null,
-    login_pass		varchar(50)  not null,
-    login_role		char(1)		 not null,
-    login_log   	varchar(100) not null,
-    FOREIGN KEY (login_userid) REFERENCES user_info(user_id),
-    FOREIGN KEY (login_email) REFERENCES user_info(user_email),
-    FOREIGN KEY (login_pass) REFERENCES user_info(user_pass),
-    FOREIGN KEY (login_role) REFERENCES user_info(user_role)
-);
+Create table `users_login` (
+`id`         int            NOT NULL primary key auto_increment,
+`name`         varchar(45)       ,
+`email`        varchar(255)    ,
+`password`     varchar(255)    ,
+`time`         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP   
+)engine = InnoDB ;
 
-insert into user_Login(login_email,login_userid,login_pass,login_role,login_log) VALUES
-('steve_jolington@gmail.com',00001,'Sjo12345','u','steve_jolington@gmail.com - 192-123-267-1 - 20/3/2022 7:52pm'),
-('peter_p@gmail.com',00002,'Ppan_01','a','peter_p@gmail.com - 192-168-167-1 - 19/3/2022 8:02am'),
-('peter_p@gmail.com',00002,'Ppan_01','a','peter_p@gmail.com - 192-168-167-1 - 20/3/2022 3:02pm');
+
 
 Create table playlist(
-	playlist_id			int(5)			primary key,
+	playlist_id			int				primary key,
     playlist_title		varchar(100)	not null,
-    playlist_bywho		varchar(45)	not null,
-    FOREIGN KEY (playlist_bywho) REFERENCES user_info(user_username)
+    playlist_bywho		varchar(45)		not null
 );
 INSERT INTO playlist(playlist_id,playlist_title,playlist_bywho) values
 (10001,'myplaylist','Steve_Jo'),
@@ -47,10 +36,10 @@ INSERT INTO playlist(playlist_id,playlist_title,playlist_bywho) values
 (10003,'Supportive list','Li_Lo');
 
 create table song_info(
-	song_id				int(8)		 	primary key,
+	song_id				int		 		primary key,
 	song_name			varchar(20)  	not null,
-    song_produced_date	date		 	not null,
-    song_rating			int(1)		 	not null,
+    song_produced_date	date		 	,
+    song_rating			int		 		,
     song_brand		   	varchar(20)  	,
     song_solo_musician	varchar(20)		,
     song_type_song		varchar(20)		,
@@ -63,26 +52,28 @@ insert into song_info(song_id,song_name,song_produced_date,song_rating,song_bran
 (00000001,'Sugar','2015-1-13','4','Maroon 5',null,'Pop','"Sugar" is a song recorded by American band Maroon 5 ..........',null,null,'V'),
 (00000002,'Imagine','1971-9-9','4',null,'John Lennon','Rock','Imagine is the second studio album by English musician John Lennon, released on 9 September 1971 by Apple Records...........',null,null,'Imagine'),
 (00000003,'Blinding light','2019-11-29','4',null,'The Weeknd','Electropop','"Blinding Lights" is a song by Canadian singer-songwriter the Weeknd. It was released on November 29, 2019,..........',null,null,'After Hours'),
-(00000004,'Sugar2','2019-11-29','4',null,'The Weeknd','Electropop','"Blinding Lights" is a song by Canadian singer-songwriter the Weeknd. It was released on November 29, 2019,..........',null,null,'After Hours');
+(00000004,'Sugar2','2015-1-13','4','Maroon 5',null,'Pop','"Sugar" is a song recorded by American band Maroon 5 ..........',null,null,'V'),
+(00000005,'Sugar3','2015-1-13','4','Maroon 5',null,'Pop','"Sugar" is a song recorded by American band Maroon 5 ..........',null,null,'V'),
+(00000006,'Sugar4','2015-1-13','4','Maroon 5',null,'Pop','"Sugar" is a song recorded by American band Maroon 5 ..........',null,null,'V');
 
 create table user_like(
-	userid			int(5)		not null,
-    songid			int(8)		not null,
+	userid			varchar(5)	not null,
+    songid			int			not null,
 	FOREIGN KEY (userid) REFERENCES user_info(user_id),
     FOREIGN KEY (songid) REFERENCES song_info(song_id)
 );
 
 ALTER TABLE user_like
   ADD PRIMARY KEY (userid,songid);
-  
+
 insert into user_like(userid,songid) VALUES
-(00001,00000001),
-(00001,00000002),
-(00003,00000003);
+('u0001',00000001),
+('u0001',00000002),
+('u0003',00000003);
 
 create table user_listened(
-	userid			int(5)		not null,
-    songid			int(8)		not null,
+	userid			varchar(5)	not null,
+    songid			int			not null,
 	FOREIGN KEY (userid) REFERENCES user_info(user_id),
     FOREIGN KEY (songid) REFERENCES song_info(song_id)
 );
@@ -91,13 +82,13 @@ ALTER TABLE user_listened
   ADD PRIMARY KEY (userid,songid);
   
 insert into user_listened(userid,songid) VALUES
-(00003,00000003),
-(00003,00000002),
-(00001,00000001);
+('u0003',00000003),
+('u0003',00000002),
+('u0001',00000001);
 
 create table user_listen_later(
-	userid			int(5)		not null,
-    songid			int(8)		not null,
+	userid			varchar(5)	not null,
+    songid			int			not null,
 	FOREIGN KEY (userid) REFERENCES user_info(user_id),
     FOREIGN KEY (songid) REFERENCES song_info(song_id)
 );
@@ -106,13 +97,13 @@ ALTER TABLE user_listen_later
   ADD PRIMARY KEY (userid,songid);
   
 insert into user_listen_later(userid,songid) VALUES
-(00002,00000002),
-(00002,00000001),
-(00001,00000003);
+('a0002',00000002),
+('a0002',00000001),
+('u0001',00000003);
 
 create table add_song_to_playlist(
-	playlistid		int(5)		not null,
-    songid			int(8)		not null,
+	playlistid		int		not null,
+    songid			int		not null,
 	FOREIGN KEY (playlistid) REFERENCES playlist(playlist_id),
     FOREIGN KEY (songid) REFERENCES song_info(song_id)
 );
@@ -126,8 +117,8 @@ insert into add_song_to_playlist(playlistid,songid) VALUES
 (10001,00000001);
 
 create table user_add_playlist(
-	userid			int(5)		not null,
-    playlistid		int(5)		not null,
+	userid			varchar(5)	not null,
+    playlistid		int			not null,
     FOREIGN KEY (userid) REFERENCES user_info(user_id),
 	FOREIGN KEY (playlistid) REFERENCES playlist(playlist_id)
 );
@@ -136,7 +127,7 @@ ALTER TABLE user_add_playlist
   ADD PRIMARY KEY (userid,playlistid);
   
 insert into user_add_playlist(userid,playlistid) VALUES
-(00003,10001),
-(00003,10002),
-(00001,10003);
+('u0003',10001),
+('u0003',10002),
+('u0001',10003);
 
